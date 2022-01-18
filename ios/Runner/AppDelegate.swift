8a7d1c25aca9181f6e915f0a492ef33795d1884c
Omar Elhamy <omar_elhamy@outlook.com>
2022-01-18 15:43:34 +0200

    asd

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,6 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBb81bbl0NKBxTox3AvkwBheP5uY6DcMSY")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
